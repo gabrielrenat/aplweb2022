bd1455c42fe272b5579c320a2edc131b9c3cba22
lazarodu <lazarodu@gmail.com>
2022-04-19 08:05:36 -0300

    aula do dia 19/03/2022

---
+++ model/Crud.php
@@ -0,0 +1,19 @@
+<?php
+class Crud
+{
+  private $tabela;
+  public function __construct($tabela)
+  {
+    $this->tabela = $tabela;
+  }
+  public function select($campos = "*", $condicao = NULL)
+  {
+    $conexao = Transaction::get();
+    if (!$condicao) {
+      $sql = "SELECT $campos FROM $this->tabela";
+    } else {
+      $sql = "SELECT $campos FROM $this->tabela WHERE $condicao";
+    }
+    $resultado = $conexao->query($sql);
+  }
+}
